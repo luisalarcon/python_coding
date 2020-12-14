5f4251c87d0cc6b6126d887b4253832e4f7356d2
Luis Alarcon <luuisalarcon@gmail.com>
2020-12-13 20:32:19 -0600

    chapter 3 challenges

---
+++ chapter3_challenges.py
@@ -0,0 +1,46 @@
+#challenge one
+print("First challenge")
+print("Python is fun")
+print("Everyday I learn and grow in different ways.")
+
+#challenge two
+print("Second challenge")
+n = 11
+if n < 10:
+    print("variable is less than 10")
+else:
+    print("variable is greater than or equal to 10")
+
+#challenge 3
+print("Third challenge")
+n = 26
+if n <= 10:
+    print("variable is less than or equal to 10")
+elif n > 10 and n <= 25 :
+    print("variable is greater than 10 and less than or equal to 25")
+else:
+    print("variable is greater than 25")
+
+#challenge 4
+print("Fourth challenge")
+a = 22
+b = 2
+print("The remainder of two constants is: ")
+print(a%b)
+
+#challenge 5
+print("Fifth challenge")
+a = 22
+b = 2
+print("The quotient of two constants is: ")
+print(a//b)
+
+#challenge 6
+age = 11
+if age < 18:
+    print("look at the little baby!")
+elif age < 21:
+    print("Okay, you are now a young adult.")
+else:
+    print("""You have made it to what people call the glory age,
+don't drink too much!""")
